722048c5258981d705c43c010f15c57bccb811f8
Max Chernikov <max.chernikoff@gmail.com>
2025-04-18 10:11:19 +0300

    Add missing packages

---
+++ packages/notification-center/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|webp|ttf|woff|woff2|svg)$': 'jest-transform-stub',
+  },
+  testPathIgnorePatterns: ['dist'],
+};
